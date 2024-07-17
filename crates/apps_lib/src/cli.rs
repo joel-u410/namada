e085194e71358204eb0231c49b36ccf16f2e100e
Joel Nordell <joel@unit410.com>
2024-07-17 18:45:04 -0500

    [feature] Allow balance querying at specified block height

--- crates/apps_lib/src/cli.rs
+++ crates/apps_lib/src/cli.rs
@@ -6088,6 +6088,7 @@ pub mod args {
                 owner: chain_ctx.get_cached(&self.owner),
                 token: chain_ctx.get(&self.token),
                 no_conversions: self.no_conversions,
+                height: self.height,
             })
         }
     }
@@ -6098,11 +6099,13 @@ pub mod args {
             let owner = BALANCE_OWNER.parse(matches);
             let token = TOKEN.parse(matches);
             let no_conversions = NO_CONVERSIONS.parse(matches);
+            let height = BLOCK_HEIGHT_OPT.parse(matches);
             Self {
                 query,
                 owner,
                 token,
                 no_conversions,
+                height,
             }
         }
 
@@ -6120,6 +6123,10 @@ pub mod args {
                 )
                 .arg(NO_CONVERSIONS.def().help(wrap!(
                     "Whether not to automatically perform conversions."
+                )))
+                .arg(BLOCK_HEIGHT_OPT.def().help(wrap!(
+                    "The block height at which to query the balance. \
+                     (Optional)"
                 )))
         }
     }
@@ -6979,6 +6986,7 @@ pub mod args {
         type AddrOrNativeToken = WalletAddrOrNativeToken;
         type Address = WalletAddress;
         type BalanceOwner = WalletBalanceOwner;
+        type BlockHeight = BlockHeight;
         type BpConversionTable = PathBuf;
         type ConfigRpcTendermintAddress = ConfigRpcAddress;
         type Data = PathBuf;
